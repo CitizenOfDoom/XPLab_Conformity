cfd9cd010a607451f70c12355e1cf5bc8385f098
llloft <sarahneuhoff@gmx.de>
2021-09-09 13:51:53 +0200

    complete code for analysis

--- Analyses/main/Analysis.R
+++ Analyses/main/Analysis.R
@@ -1,25 +1,86 @@
-#install.packages("Bridge Sampling")
-#library(bridgesampling)
-#bf(x1, x2, log = FALSE, ...)
-devtools::install_github("tidyverse/readr")
-library(tidyverse)
-library(brms)
+#analysis_outgroup_plan
+install.packages("sjmisc")
+install.packages("here")
+#Iniial set-up ---------------
+library(sjmisc)
+library(here)
 library(ordinal)
+library(tidyverse)
 library(rstan)
 library(bridgesampling)
-data <- read_csv("C:/Users/sarah/Desktop/psylab/clean_df.csv")
+library(shinystan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+set.seed(123)
 
-     
-as_tibble(data)
-view(data)
-# condition 1: only ingroup norm, report robber
-# condition 2: only ingroup norm, let robber go
-# condtion 3: both norms, report robber
-# condition 4: both norms, let robber go
-data %>% mutate(ingroup_agree=)
-data %>% mutate(log_odds=b_in*ingroup_agree + b_both + b_out*outgroup_agree)
-fit_model <- brm(
-  formula=condition ~ log_odds,
-  data=data,
-  prior=
-)
+
+#--Set APA theme to use with ggplot
+theme_set(theme_bw(18)+
+            theme(panel.grid.major=element_blank(),
+                  panel.grid.minor=element_blank(),
+                  panel.border=element_blank(),
+                  strip.background=element_blank(),
+                  strip.text = element_text(face="bold"),
+                  axis.line=element_line(),
+                  text=element_text(family="serif")))
+
+#Read in data
+full_data <- read_csv(here("XPLab_Conformity","Analyses","main","df_for_modeling.csv"))
+
+
+#Create readable labels for plotting.
+#full_data <- mutate(full_data, condition = ifelse(ingroupNorm==1 & bothShown==0, "Ingroup norm favoured \n leaving robber alone \n Only ingroup norm shown",
+                                                  #ifelse(ingroupNorm==0 & bothShown==0, "Ingroup norm favoured \n calling the police \n Only ingroup norm shown",
+                                                         #ifelse(ingroupNorm==1 & bothShown==1, "Ingroup norm favoured \n leaving robber alone \n Both norms shown",
+                                                               # "Ingroup norm favoured \n calling the police \n Both norms shown"))))
+#full_data$responseLabels <- ordered(full_data$response, labels = c("1 = Definitely call the police", "2", "3", "4", "5", "6 = Definitely leave the robber alone"))
+
+
+
+full_data["preference"][full_data["preference"] == -3] <- 1
+full_data["preference"][full_data["preference"] == -2] <- 2
+full_data["preference"][full_data["preference"] == -1] <- 3
+full_data["preference"][full_data["preference"] == 1] <- 4
+full_data["preference"][full_data["preference"] == 2] <- 5
+full_data["preference"][full_data["preference"] == 3] <- 6
+full_data["response"] <- full_data["preference"]
+View(full_data)
+#Frequentist Analysis----------------
+ordinal <- clm(as.factor(preference)~ingroupNorm*bothShown, data=full_data)
+summary(ordinal)
+
+
+
+
+
+#Bayesian analysis------------
+#--Set up data for stan.
+useable_data <- full_data %>%
+  select(response, ingroupNorm, bothShown, ingroupAgree, outgroupDisagree) %>% #select relevant columns
+  mutate(ingroupAgree = ifelse(is.na(ingroupAgree), 1, ingroupAgree), #For experiments 1 and 2 we assumed that participants identified with the ingroup and not with the outgroup.
+         outgroupDisagree = ifelse(is.na(outgroupDisagree), 1, outgroupDisagree)
+         )
+stan_data_all <- as.list(c(useable_data, N = dim(useable_data)[1]))
+
+
+print(stan_data_all)
+#--Fit model
+
+fit_SCT_all <- stan(file = here("XPLab_Conformity","Analyses","main","stan_models","SCT.stan"), data=stan_data_all, iter=10000, chains=1, seed = 123, control=list(adapt_delta = 0.99))
+fit_herding_all <- stan(file = here("XPLab_Conformity","Analyses","main","stan_models","SCT.stan"), data=stan_data_all, iter=10000, chains=1, control=list(adapt_delta = 0.99))
+
+
+#----Check diagnostics
+#launch_shinystan(fit_SCT_all)
+
+#--Compare models using Bayes Factors
+
+marg_lik_SCT_all <- bridge_sampler(samples = fit_SCT_all)
+marg_lik_herding_all <- bridge_sampler(samples = fit_herding_all)
+bf(marg_lik_herding_all,marg_lik_SCT_all)
+
+
+#Create prior-posterior plots-------
+#source("C:/Users/sarah/Documents/XPLab_Conformity/Analyses/main/plot_priors_posteriors.R")
+
+
