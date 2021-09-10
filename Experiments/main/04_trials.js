3a8f614c21b0daf742fc227381795bbd205f97ca
CitizenOfDoom <44921205+CitizenOfDoom@users.noreply.github.com>
2021-09-10 23:51:00 +0200

    Update 04_trials.js

--- Experiments/main/04_trials.js
+++ Experiments/main/04_trials.js
@@ -2,22 +2,6 @@
 
 
 const trial_info = {
-    forced_choice: [
-        {
-            question: "What's on the bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
-        },
-        {
-            question: "What's the weather like?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
-        }
-    ],
     sentence_choice_trials: [
       {
         question: "Which out of these topics do you care about the most?",
